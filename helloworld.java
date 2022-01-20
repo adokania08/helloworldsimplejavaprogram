8dc58ede579afc305f0b074852dd4882766cae42
adokania08 <97380070+adokania08@users.noreply.github.com>
2022-01-20 16:51:23 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch23 team");
+        System.out.println("Hello batch23  1 team");
       System.out.println("I am Abhishek from batch 23 ");
     }  
 } 
